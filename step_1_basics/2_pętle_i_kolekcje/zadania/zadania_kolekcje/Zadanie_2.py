600205900a4a52b319df9dfeed1f5d73323673a0
Szymon Kotlowski <szymonkotlowski20@gmail.com>
2022-06-25 15:05:45 +0200

    Day 1 update

--- step_1_basics/2_pętle_i_kolekcje/zadania/zadania_kolekcje/Zadanie_2.py
+++ step_1_basics/2_pętle_i_kolekcje/zadania/zadania_kolekcje/Zadanie_2.py
@@ -5,3 +5,13 @@ i dodaje je do listy dopóki użytkownik nie poda wartości 'nie'
 
 Wypisz listę na ekran.
 """
+
+lista = []
+
+while True:
+    value = input('Podaj jakas wartosc: ')
+    if value == 'nie':
+        break
+    lista.append(value)
+
+print(lista)
